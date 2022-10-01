c37b4fc8193b781f82f3f03c12b4d7c3f29916ae
HasanAbdirahman <108181341+HasanAbdirahman@users.noreply.github.com>
2022-10-01 07:53:19 -0400

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const third = document.querySelector(".three");
 const mark = document.querySelector(".mark");
 const section = document.querySelectorAll(".section");
 const dott = document.querySelector(".dot");
+let newSection;
+
 mark.addEventListener("click", () => {
   for (const each of section) {
     each.classList.remove("section");
@@ -12,24 +14,22 @@ mark.addEventListener("click", () => {
     dott.style.display = "none";
   }
 });
-// first.addEventListener("click", () => {
-//   document.querySelector(".circle").innerText = section.length - 1;
-//   first.classList.remove("section");
-//   first.classList.add("new-section");
-
-for (let i = 0; i < section.length; i--) {
-  document.querySelectorAll(".circle").innerText = section.length - 1;
+first.addEventListener("click", () => {
+  document.querySelector(".circle").innerText = section.length - 1;
+  first.classList.remove("section");
+  first.classList.add("new-section");
+  dott.style.display = "none";
+});
+second.addEventListener("click", () => {
+  document.querySelector(".circle").innerText = section.length - 2;
   second.classList.remove("section");
   second.classList.add("new-section");
-}
-// });
-// second.addEventListener("click", () => {
-//   document.querySelector(".circle").innerText = section.length - 1;
-//   second.classList.remove("section");
-//   second.classList.add("new-section");
-// });
-// third.addEventListener("click", () => {
-//   document.querySelector(".circle").innerText = section.length - 1;
-//   third.classList.remove("section");
-//   third.classList.add("new-section");
-// });
+  dott.style.display = "none";
+});
+
+third.addEventListener("click", () => {
+  document.querySelector(".circle").innerText = section.length - 3;
+  third.classList.remove("section");
+  third.classList.add("new-section");
+  dott.style.display = "none";
+});
